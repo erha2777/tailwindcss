2f0b0d1bf147d173260879fbf553adee781c5c22
v_jaweiwang <v_jaweiwang@tencent.com>
2024-11-12 17:56:34 +0800

    feat: 引入tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
+import tailwindcss from  'tailwindcss'
+import autoprefixer from 'autoprefixer'
+
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src')
+    }
+  },
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss, 
+        autoprefixer,
+      ]
+    }
+  }
 })
