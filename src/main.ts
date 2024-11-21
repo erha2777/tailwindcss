080b5df4eaab9a5f25e64e32352dca797e605e04
v_jaweiwang <v_jaweiwang@tencent.com>
2024-11-21 15:39:05 +0800

    feat: 使用全屏滚动插件

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
+import './styles/common.css'
 import './tailwind.css'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import VueFullpage from 'vue-fullpage.js';
+createApp(App).use(VueFullpage).mount('#app')
